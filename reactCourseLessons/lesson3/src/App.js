f639df3471f09efd61acce1a63186945b717ad7e
Jen <gk_ln@mail.ru>
2021-09-13 09:38:49 +0300

    lesson 3 part 2

--- reactCourseLessons/lesson3/src/App.js
+++ reactCourseLessons/lesson3/src/App.js
@@ -1,98 +1,101 @@
 import React, { useState } from 'react';
 import Counter from './Counter';
 
+import useWindowSize from "../hooks/useWindowSize";
+
 const productFromServer = [
-	{
-		id: 100,
-		title: 'Ipnone 200',
-		price: 12000,
-		rest: 10,
-		cnt: 1
-	},
-	{
-		id: 101,
-		title: 'Samsung AAZ8',
-		price: 22000,
-		rest: 2,
-		cnt: 1
-	},
-	{
-		id: 103,
-		title: 'Nokia 3310',
-		price: 5000,
-		rest: 2,
-		cnt: 1
-	},
-	{
-		id: 105,
-		title: 'Huawei ZZ',
-		price: 15000,
-		rest: 8,
-		cnt: 1
-	}
+    {
+        id: 100,
+        title: 'Ipnone 200',
+        price: 12000,
+        rest: 10,
+        cnt: 1
+    },
+    {
+        id: 101,
+        title: 'Samsung AAZ8',
+        price: 22000,
+        rest: 2,
+        cnt: 1
+    },
+    {
+        id: 103,
+        title: 'Nokia 3310',
+        price: 5000,
+        rest: 2,
+        cnt: 1
+    },
+    {
+        id: 105,
+        title: 'Huawei ZZ',
+        price: 15000,
+        rest: 8,
+        cnt: 1
+    }
 ];
 
-function App(){
-	let [products, setProducts] = useState(productFromServer);
-	let total = products.reduce((acc, pr) => acc + pr.price * pr.cnt, 0);
+function App() {
+    let { width } = useWindowSize();
+    let [products, setProducts] = useState(productFromServer);
+    let total = products.reduce((acc, pr) => acc + pr.price * pr.cnt, 0);
 
-	let changeCnt = (id, cnt) => {
-		setProducts(products.map(pr => pr.id !== id ? pr : {...pr, cnt}));
-	}
+    let changeCnt = (id, cnt) => {
+        setProducts(products.map(pr => pr.id !== id ? pr : { ...pr, cnt }));
+    }
 
-	let remove = (id) => {
-		setProducts(products.filter(pr => pr.id !== id));
-	}
+    let remove = (id) => {
+        setProducts(products.filter(pr => pr.id !== id));
+    }
 
-	let tmpSaleAllBy5 = () => {
-		setProducts(products.map(pr => ({...pr, cnt: 5})));
-	}
+    let tmpSaleAllBy5 = () => {
+        setProducts(products.map(pr => ({ ...pr, cnt: 5 })));
+    }
 
-	let productsRows = products.map(pr => (
-		<tr key={pr.id}>
-			<td>{ pr.title }</td>
-			<td>{ pr.price }</td>
-			<td>
-				<Counter max={100} current={pr.cnt} onChange={val => changeCnt(pr.id, val)} />
-			</td>
-			<td>{ pr.price * pr.cnt }</td>
-			<td>
-				<button  type="button" onClick={()=> remove(pr.id)}>X</button>
-			</td>
-		</tr>
-	));
+    let productsRows = products.map(pr => (
+        <tr key={ pr.id }>
+            <td>{ pr.title }</td>
+            <td>{ pr.price }</td>
+            <td>
+                <Counter max={ pr.rest } current={ pr.cnt } onChange={ val => changeCnt(pr.id, val) }/>
+            </td>
+            { width > 700 && <td>{ pr.price * pr.cnt }</td> }
+            <td>
+                <button type="button" onClick={ () => remove(pr.id) }>X</button>
+            </td>
+        </tr>
+    ));
 
-	return <div>
-		<header>
-			header
-		</header>
-		<main>
-			<h1>Cart</h1>
-			<hr/>
-			<table>
-				<thead>
-					<tr>
-						<td>Title</td>
-						<td>Price</td>
-						<td>Count</td>
-						<td>Total</td>
-					</tr>
-				</thead>
-				<tbody>
-					{ productsRows }
-				</tbody>
-			</table>
-			<hr/>
-			<div>
-				<strong>{ total }</strong>
-			</div>
-			<hr/>
-			<button  type="button" onClick={tmpSaleAllBy5}>Want all 5</button>
-		</main>
-		<footer>
-			footer
-		</footer>
-	</div>
+    return <div>
+        <header>
+            header
+        </header>
+        <main>
+            <h1>Cart</h1>
+            <hr/>
+            <table>
+                <thead>
+                <tr>
+                    <td>Title</td>
+                    <td>Price</td>
+                    <td>Count</td>
+                    { width > 700 && <td>Total</td> }
+                </tr>
+                </thead>
+                <tbody>
+                { productsRows }
+                </tbody>
+            </table>
+            <hr/>
+            <div>
+                <strong>{ total }</strong>
+            </div>
+            <hr/>
+            <button type="button" onClick={ tmpSaleAllBy5 }>Want all 5</button>
+        </main>
+        <footer>
+            footer
+        </footer>
+    </div>
 }
 
 export default App;
--- reactCourseLessons/lesson3/src/Counter.js
+++ reactCourseLessons/lesson3/src/Counter.js
@@ -1,44 +1,51 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
+import useWindowSize from '../hooks/useWindowSize';
 
 Counter.propTypes = {
-	min: PropTypes.number,
-	max: PropTypes.number.isRequired,
-	current: PropTypes.number.isRequired,
-	onChange: PropTypes.func.isRequired
+    min: PropTypes.number,
+    max: PropTypes.number.isRequired,
+    current: PropTypes.number.isRequired,
+    onChange: PropTypes.func.isRequired
 }
 
 Counter.defaultProps = {
-	min: 1
+    min: 1
 }
 
-function Counter({ min, max, current, onChange }){
-	let [value, setValue] = useState((current));
-	let inpStr = (e) => setValue(e.target.value);
+function Counter({ min, max, current, onChange }) {
+    let { width } = useWindowSize();
+    let inp = useRef();
+    let updInp = num => inp.current.value = num;
 
-	let dec = () => applyCurrent(current - 1);
-	let inc = () => applyCurrent(current + 1);
+    let dec = () => applyCurrent(current - 1);
+    let inc = () => applyCurrent(current + 1);
 
-	let applyStrValue = () => {
-		let val = parseInt(value);
-		applyCurrent(isNaN(val) ? min : val);
-	}
+    let applyStrValue = (e) => {
+        let val = parseInt(e.target.value);
+        applyCurrent(isNaN(val) ? min : val);
+    }
 
-	let applyCurrent = (number) => {
-		let newCurrent = Math.max(min, Math.min(number, max));
-		onChange(newCurrent);
-		setValue(newCurrent);
-	}
+    let applyCurrent = (number) => {
+        let newCurrent = Math.max(min, Math.min(number, max));
+        updInp(number);
 
-	useEffect(() => {
-		setValue(current);
-	}, [current]);
+        if (current !== newCurrent) {
+            onChange(newCurrent);
+        }
+    }
 
-	return <div>
-		<button type="button" onClick={dec} disabled={current <= min}>-</button>&nbsp;
-		<input value={value} onChange={inpStr} onBlur={applyStrValue}/>&nbsp;
-		<button type="button" onClick={inc} disabled={current >= max}>+</button>
-	</div>
+    // change from parent side
+    useEffect(() => {
+        updInp(current);
+    }, [current]);
+
+    return <div>
+        <button type="button" onClick={ dec } disabled={ current <= min }>-</button>
+        &nbsp;
+        <input ref={ inp } defaultValue={ current } onBlur={ applyStrValue } size={ width > 500 ? 5 : 2 }/>&nbsp;
+        <button type="button" onClick={ inc } disabled={ current >= max }>+</button>
+    </div>
 }
 
 export default Counter;
